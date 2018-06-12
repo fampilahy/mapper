54ea0d7eac55566f67aa32969829236e896c233c
harihaja.ampilahy <harihaja.ampilahy@banregio.com>
2018-06-12 17:23:18 -0500

    fix objectMapper

--- src/tools/JsonTool.java
+++ src/tools/JsonTool.java
@@ -1,16 +1,19 @@
 package tools;
+
+import java.io.File;
+import java.io.IOException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import model.document.AbstractDocument;
-import model.document.chubb.ChubbDocument;
 
 public class JsonTool {
-	
-	public static JsonNode fromDocumentToJsonNode(AbstractDocument document){
+
+	public static final ObjectMapper objectMapper = new ObjectMapper();
+
+	public static JsonNode fromDocumentToJsonNode(AbstractDocument document) {
 		JsonNode jsonNode = null;
-		if(document == null) return null;
-		ObjectMapper objectMapper = new ObjectMapper();
+		if (document == null)
+			return null;
 		try {
 			jsonNode = objectMapper.readTree(objectMapper.writeValueAsString(document));
 		} catch (Exception e) {
@@ -18,13 +21,26 @@ public class JsonTool {
 		}
 		return jsonNode;
 	}
-	
-	public static AbstractDocument fromJsonNodeToDocument(JsonNode jsonNode, ChubbDocument chubbDocument){
-//		chubbDocument.getClass();
-		return null;
-		
+
+	public static AbstractDocument fromJsonNodeToDocument(JsonNode jsonNode, AbstractDocument abstractDocument) {
+		AbstractDocument document = null;
+		try {
+			document = objectMapper.readValue(objectMapper.writeValueAsString(jsonNode), abstractDocument.getClass());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return document;
 	}
-	
-	
+
+	public static AbstractDocument fromFileJsonNodeToDocument(String filePath, AbstractDocument abstracDocument) {
+		AbstractDocument document = null;
+		try {
+			File file = new File(filePath);
+			document = objectMapper.readValue(file, abstracDocument.getClass());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return document;
+	}
+
 }
-	
