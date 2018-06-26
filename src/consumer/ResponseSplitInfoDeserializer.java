d7104e16e8391f27b0f14c5ad4f71d5ed559f260
harihaja.ampilahy <harihaja.ampilahy@banregio.com>
2018-06-26 15:31:52 -0500

    a

--- src/consumer/ResponseSplitInfoDeserializer.java
+++ src/consumer/ResponseSplitInfoDeserializer.java
@@ -5,29 +5,40 @@ import java.util.Map;
 
 import model.document.chubb.splitInfo.response.ResponseSplitInfo;
 
-public class ResponseSplitInfoDeserializer extends AbstractDeserializer<ResponseSplitInfo>{
+public class ResponseSplitInfoDeserializer extends AbstractDeserializer<ResponseSplitInfo> {
 
-	
-	//this is a subject of memory usage, would implement a way to free up memory next time anyway the objects used are not too voluminous
+	private static final String DIRECTORY = "src";
+	private static final String PATH_SEPARATOR = "/";
+	private static final String FILE_EXTENSION = ".json";
+
+	// this is a subject of memory usage, would implement a way to free up
+	// memory next time anyway the objects used are not too voluminous
 	public static final Map<String, ResponseSplitInfo> responsesSplitInfo = new HashMap<String, ResponseSplitInfo>();
-
-
 
 	@Override
 	public ResponseSplitInfo getObject(String key) {
-		
-		if (key==null || key.equals("")) return null;
+
+		if (key == null || key.equals(""))
+			return null;
 		ResponseSplitInfo responseSplitInfo = null;
-		if(responsesSplitInfo.containsKey(key)){
+		if (responsesSplitInfo.containsKey(key)) {
 			return responsesSplitInfo.get(key);
-		}else{
-			//process check on db or on file
+		} else {
+
+			// process check on db or on file
 		}
 		return responseSplitInfo;
-		
+
 	}
-	
-	
-	
+
+	private static void insetObjectToMemory(String key) {
+		if (key == null)
+			return;
+
+	}
+
+	private static String pathBuilder(String key) {
+		return (key == null || key.equals("")) ? null : null;
+	}
 
 }
